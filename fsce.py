613c8100431dfe2456fa1c5f9bc36e5b6ce51c44
IaMRiZzY28 <28ragavn@gmail.com>
2024-09-18 18:19:33 +0530

    idk

---
+++ fsce.py
@@ -0,0 +1,12 @@
+my_dict = {'name':'jack','age': 26}
+print(my_dict)
+print(my_dict['age'])
+my_dict["address"] ="pune"
+print(my_dict)
+my_dict.pop('name')
+print(my_dict)
+my_dict.clear()
+print(my_dict)
+
+my_tuple =("ragav",11)
+print(my_tuple)
